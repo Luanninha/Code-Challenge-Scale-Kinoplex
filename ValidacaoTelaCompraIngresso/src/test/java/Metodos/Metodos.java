ba8cd97258e5928f792d7094b000a6baae32aac1
Luanninha <luana.luli@hotmail.com>
2024-08-30 13:00:52 -0300

    Alterações

--- ValidacaoTelaCompraIngresso/src/test/java/Metodos/Metodos.java
+++ ValidacaoTelaCompraIngresso/src/test/java/Metodos/Metodos.java
@@ -1,12 +1,11 @@
 package Metodos;
 
-import static org.junit.Assert.assertEquals;
+
 
 
 import java.io.File;
 import java.io.IOException;
-import java.time.Duration;
-import java.util.List;
+
 
 import java.util.NoSuchElementException;
 
@@ -17,9 +16,9 @@ import org.openqa.selenium.TakesScreenshot;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
-import org.openqa.selenium.support.ui.ExpectedConditions;
+
 import org.openqa.selenium.support.ui.Select;
-import org.openqa.selenium.support.ui.WebDriverWait;
+
 
 
 public class Metodos {
@@ -70,17 +69,6 @@ public class Metodos {
 		driver.findElement(elemento).click();
 	}
 
-	// Método para selecionar dia
-	public void selecionarDia(String dia) {
-		List<WebElement> dias = driver.findElements(By.cssSelector(".react-datepicker__day"));
-		for (WebElement e : dias) {
-			if (e.getText().equals(dia)) {
-				e.click();
-				return;
-			}
-		}
-		throw new NoSuchElementException("Dia " + dia + " não encontrado no calendário.");
-	}
 
 	// Método para preencher
 	public void preencher(By elemento, String nome) {
@@ -103,14 +91,7 @@ public class Metodos {
 		driver.quit();
 	}
 
-	// Método para validar texto
-	public void validarTexto(By elemento, String textoEsperado) {
-		String mensagemCapturada = driver.findElement(elemento).getText().trim();
-		mensagemCapturada = mensagemCapturada.replaceAll("\\s+", " ");
-		textoEsperado = textoEsperado.trim().replaceAll("\\s+", " ");
-		System.out.println("Texto Capturado: [" + mensagemCapturada + "]");
-		assertEquals(textoEsperado, mensagemCapturada);
-	}
+
 
 	// Método para tirar print
 	public void tirarPrint(String nome) throws IOException {
@@ -118,34 +99,6 @@ public class Metodos {
 		File SrcFile = print.getScreenshotAs(OutputType.FILE);
 		File Desfile = new File("./src/evidencias/" + nome + ".png");
 		FileUtils.copyFile(SrcFile, Desfile);
-	}
-
-	// Método para forçar clique com javascript
-	public void clicarElementoComJavaScript(By elemento) {
-		rolarAteElemento(elemento);
-		WebElement webElement = driver.findElement(elemento);
-		((org.openqa.selenium.JavascriptExecutor) driver).executeScript("arguments[0].click();", webElement);
-	}
-
-
-
-	// Método para rolar a página até um elemento
-	public void rolarAteElemento(By elemento) {
-		((org.openqa.selenium.JavascriptExecutor) driver).executeScript("arguments[0].scrollIntoView(true);",
-				driver.findElement(elemento));
-	}
-
-
-
-	// Método para alternar para uma nova janela
-	public void mudarParaNovaJanela() {
-		String janelaAtual = driver.getWindowHandle();
-		for (String janela : driver.getWindowHandles()) {
-			if (!janela.equals(janelaAtual)) {
-				driver.switchTo().window(janela);
-				break;
-			}
-		}
 	}
 
 
